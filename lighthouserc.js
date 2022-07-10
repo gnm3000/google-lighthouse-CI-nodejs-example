3740f983c4ce65faaab2b364eed36e02966c424a
German N Montenegro <gnm3000@gmail.com>
2022-07-10 12:32:38 -0300

    token

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,12 +3,23 @@ module.exports = {
     collect: {
         url: ['http://localhost:8080/'],
         startServerCommand: 'npm start',
+        numberOfRuns:3,
         },
+    assert: {
+        //preset: 'lighthouse:recommended',
+        "assertions": {
+            "categories:performance": ["warn", {"minScore": 0.9}],
+            "categories:accessibility": ["error", {"minScore": 1}]
+          }
+    },
     upload: {
         target: 'temporary-public-storage',
       },
-    assert: {
-        preset: 'lighthouse:recommended'
-    }
+    server: {
+        // server options here
+      },
+      wizard: {
+        // wizard options here
+      },
     },
   };
